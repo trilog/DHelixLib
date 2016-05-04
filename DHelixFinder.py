294433ccd01f2c55070e9a2632991fb32a7ff95b
Damian Reynolds <damian.reynolds@triangular-logic.com>
2016-05-04 15:30:06 -0400

    Dual plots

--- DHelixFinder.py
+++ DHelixFinder.py
@@ -4,26 +4,38 @@ import csv
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-xs = list()
-ys = list()
-zs = list()
+dataX = list()
+dataY = list()
+dataZ = list()
 
 with open("input.tsv", "r", newline = '') as inFile:
     csvreader = csv.reader(inFile, delimiter = '\t')
     for row in csvreader:
-        xs.append(float(row[0]))
-        ys.append(float(row[1]))
-        zs.append(float(row[2]))
+        dataX.append(float(row[0]))
+        dataY.append(float(row[1]))
+        dataZ.append(float(row[2]))
 
+#fig = plt.figure(figsize=plt.figaspect(0.5))
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+
+ax = fig.add_subplot(121, projection='3d')
+ax.set_aspect(1)
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 ax.set_xlim(-10, 10)
 ax.set_ylim(-10, 10)
 ax.set_zlim(-10, 10)
+ax.scatter(dataX, dataY, dataZ)
 
-ax.scatter(xs, ys, zs)
+a2 = fig.add_subplot(122, projection="3d")
+a2.set_aspect(1)
+a2.set_xlabel('X')
+a2.set_ylabel('Y')
+a2.set_zlabel('Z')
+a2.set_xlim(-10, 10)
+a2.set_ylim(-10, 10)
+a2.set_zlim(-10, 10)
+a2.plot(dataX, dataY, dataZ)
 
 plt.show()
